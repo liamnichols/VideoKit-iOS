ba09bf0bad3c915506b97f2995d09e6792705e0d
Dennis Stücken <dstuecken@me.com>
2021-02-10 20:24:51 -0800

    updating checksum

--- Package.swift
+++ Package.swift
@@ -31,13 +31,15 @@ let package = Package(
             targets: ["VideoKitLiveWrapper"]
         ),
     ],
-    dependencies: [],
+    dependencies: [
+        .package(name: "HaishinKit", url: "https://github.com/shogo4405/HaishinKit.swift", from: "1.1.2")
+    ],
     targets: [
         // swift package compute-checksum VideoKit-1.0.3.zip
         .binaryTarget(
             name: "VideoKitCore",
             url: "https://cdn.video.io/ios/\(version)/VideoKitCore.xcframework.zip?ignoreCache=1",
-            checksum: "ff57b8eeefa6ae089baa8b169264f530bbc9ca99a495d17443a57a50b30ebb5c"
+            checksum: "0a9045107e6a7f33b38c27d980ed795ff0156216a04d0f6d4193bbe9eb881a31"
         ),
         .target(
               name: "VideoKitCoreWrapper",
@@ -52,22 +54,23 @@ let package = Package(
         .binaryTarget(
             name: "VideoKitPlayer",
             url: "https://cdn.video.io/ios/\(version)/VideoKitPlayer.xcframework.zip?ignoreCache=1",
-            checksum: "2fff2622e000d167f294608d575fecbfb812cd9ec9d23024c37e4b285e1df97d"
+            checksum: "bfdd9a42eb4eb161f3d2c9d5ea039b557b641eeafa4e76e909e3bbd60f49a47f"
         ),
         .binaryTarget(
             name: "VideoKitRecorder",
             url: "https://cdn.video.io/ios/\(version)/VideoKitRecorder.xcframework.zip?ignoreCache=1",
-            checksum: "178252275145928a1d78312882beab9d0963adadaba9fb7207bf65a04c3b0b91"
+            checksum: "9f6f5a96beec54f8f180f02b735199be0e9d415460e751d987aeeaeabfb88cb6"
         ),
         .binaryTarget(
             name: "VideoKitLive",
             url: "https://cdn.video.io/ios/\(version)/VideoKitLive.xcframework.zip?ignoreCache=1",
-            checksum: "e1aa6e96892e86b0bef4a60dc8a6d2b8918c781a47f70cbb768acb55736d6959"
+            checksum: "c79e33cbb8ff5389ad7288989a0c14df6b1db8ff383ad9af1d1ccf3744130e25"
         ),
         .target(
             name: "VideoKitLiveWrapper",
             dependencies: [
                 .target(name: "VideoKitLive"),
+                "HaishinKit",
             ],
             path: "VideoKitLiveWrapper"
         ),
