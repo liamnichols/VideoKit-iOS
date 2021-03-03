21b76ff073066240e23ff885b3890c75d9d43a70
Dennis Stücken <dstuecken@me.com>
2021-03-02 21:30:01 -0800

    1.0.7
    Changelog:
    - 1.0.7 comes with dSYM debug symbols and MacOS arm64 builds

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let version = "1.0.6"
+let version = "1.0.7"
 
 let package = Package(
     name: "VideoKit",
@@ -37,7 +37,7 @@ let package = Package(
         .binaryTarget(
             name: "VideoKitCore",
             url: "https://cdn.video.io/ios/\(version)/VideoKitCore.xcframework.zip?ignoreCache=1",
-            checksum: "bbb572c7a69637b4b43881ab2b30d50850a546a3cef8fda573c998cdcf17d481"
+            checksum: "6e4751c1b1236241b6db7689ce6a44dc3ffdb4ce48c70309103018f4e448c329"
         ),
         .target(
               name: "VideoKitCoreWrapper",
@@ -52,17 +52,17 @@ let package = Package(
         .binaryTarget(
             name: "VideoKitPlayer",
             url: "https://cdn.video.io/ios/\(version)/VideoKitPlayer.xcframework.zip?ignoreCache=1",
-            checksum: "8d236c011fd3484f1d4ea64f630da5a73a8e03134b9e960ab788d415a2ff9c2e"
+            checksum: "adfe18128fa829d32492b2cb8379c9b2cd0e245ad1cf5bdf4e1ac81ee1e5d30a"
         ),
         .binaryTarget(
             name: "VideoKitRecorder",
             url: "https://cdn.video.io/ios/\(version)/VideoKitRecorder.xcframework.zip?ignoreCache=1",
-            checksum: "b26968fcfa89c77aeee3f9ea3965be6eeab5ce4d83b757a56d01ca3af170c6a3"
+            checksum: "cadeb99f0b75369366af506fda50fb18fed2b1d33c2953629a248cc333f264da"
         ),
         .binaryTarget(
             name: "VideoKitLive",
             url: "https://cdn.video.io/ios/\(version)/VideoKitLive.xcframework.zip?ignoreCache=1",
-            checksum: "95bf403c5cd8331a6d93db8c669f2490ed558f72629e110bf8425d516271e49f"
+            checksum: "0073275433bb6ceb0f8972935bcd5585667f751dfce74228a9944c20fbe4a701"
         ),
         .target(
             name: "VideoKitLiveWrapper",
